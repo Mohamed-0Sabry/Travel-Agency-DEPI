3e48fcf1284be53493fbadb7eb210c54343dd4ca
mazen <mazenhaytham26@gmail.com>
2025-11-26 22:16:44 +0200

    some syntax fixes

--- src/types/Flight.ts
+++ src/types/Flight.ts
@@ -1,9 +1,9 @@
-import type { Destination } from "./destination";
+import type { Destination } from "./Destination";
 import type { Offer } from "./Offer";
 import type { Origin } from "./Origin";
 
 export interface Flight {
-    _id:string,
+  _id:string,
   price: number;
   origin: Origin;
   destination: Destination;
